0d43a49709aad0c6453bc80fcaec1e9ca263d46b
Andrey <metalhead190113@gmail.com>
2016-09-21 12:21:50 +0600

    Initial Commit

---
+++ Goods AccountingTests/Goods_AccountingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Goods_AccountingTests.swift
+//  Goods AccountingTests
+//
+//  Created by Andrey Sobko on 21.09.16.
+//  Copyright © 2016 Andrey Sobko. All rights reserved.
+//
+
+import XCTest
+@testable import Goods_Accounting
+
+class Goods_AccountingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
