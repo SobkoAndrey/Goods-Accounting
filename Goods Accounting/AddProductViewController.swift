a4f8454ac5f8042f5c7d243714bcec060289e87c
Andrey <metalhead190113@gmail.com>
2016-09-22 14:46:19 +0600

    Add image selection and processing keyboard in AddProductViewController

--- Goods Accounting/AddProductViewController.swift
+++ Goods Accounting/AddProductViewController.swift
@@ -22,12 +22,85 @@ class AddProductViewController: UIViewController {
         }
     }
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var typeTextField: UITextField!
     @IBOutlet weak var priceTextField: UITextField!
     @IBOutlet weak var countTextField: UITextField!
     
+    @IBAction func addPhotoButtonTouchUpInside(sender: UIButton?) {
+        // Creation image picker. Transition to photo library.
+        if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = .PhotoLibrary
+            
+            presentViewController(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - Keyboard
+    
+    @IBAction func tapOnView(sender: AnyObject?) {
+        // Remove keyboard.
+        view.endEditing(false)
+    }
+    
+    func registerForKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(
+            self,
+            selector: #selector(AddProductViewController.keyboardWillChangeFrame(_:)),
+            name: UIKeyboardWillChangeFrameNotification,
+            object: nil
+        )
+    }
+    
+    func keyboardWillChangeFrame(notification: NSNotification) {
+        
+        // Offset view in relation to keyboard.
+        
+        guard let info = notification.userInfo else { return }
+        
+        guard let finish = info[UIKeyboardFrameEndUserInfoKey]?.CGRectValue
+            else { return }
+        
+        UIView.beginAnimations(nil, context: nil)
+        
+        if let duration = info[UIKeyboardAnimationDurationUserInfoKey]?.doubleValue {
+            UIView.setAnimationDuration(duration)
+        }
+        
+        if let animationID = info[UIKeyboardAnimationCurveUserInfoKey]?.integerValue,
+            let animationCurve = UIViewAnimationCurve(rawValue: animationID) {
+            UIView.setAnimationCurve(animationCurve)
+        }
+        
+        UIView.setAnimationBeginsFromCurrentState(true)
+        
+        scrollView.contentInset = UIEdgeInsets(top: scrollView.contentInset.top, left: 0.0,
+                                                bottom: UIScreen.mainScreen().bounds.height - finish.minY, right: 0.0)
+        scrollView.scrollIndicatorInsets = scrollView.contentInset
+        
+        UIView.commitAnimations()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        registerForKeyboardNotifications()
+    }
+}
+
+extension AddProductViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // Add selected image.
+        photoImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        photoImageView.contentMode = .ScaleAspectFill
+        photoImageView.clipsToBounds = true
+        
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
--- Goods Accounting/AppDelegate.swift
+++ Goods Accounting/AppDelegate.swift
@@ -17,6 +17,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        let vc = AddProductViewController(nibName: "AddProductView", bundle: nil)
+        let nvc = UINavigationController(rootViewController: vc)
+        
+        window?.rootViewController = nvc
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
